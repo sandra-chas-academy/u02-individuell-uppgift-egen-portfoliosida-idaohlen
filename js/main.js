8ff8f1c0e14317dd02b23289d1d23e34196a17e7
idaohlen <ida-alexandra.ohlen@chasacademy.se>
2024-10-29 22:09:11 +0100

    get data from json file for about section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const projectsElement = document.querySelector(".projects-container");
+const educationList = document.querySelector(".education-list");
+const workList = document.querySelector(".work-list");
 
+// Load GitHub repos for Projects section
 function fetchGitHubRepos() {
 const loader = document.querySelector(".projects-loader");
 loader.style.display = "block";
@@ -12,7 +15,7 @@ loader.style.display = "block";
     const repos =  data.filter(repo => repo.name !== "idaohlen");
 
     repos.forEach(repo => {
-      const languages = fetch(repo.languages_url)
+      fetch(repo.languages_url)
       .then(response => response.json())
       .then(
         languages => {
@@ -27,7 +30,33 @@ loader.style.display = "block";
 
     loader.style.display = "none";
   })
-  .catch( error => console.error(error));
+  .catch(error => console.error(error));
+}
+
+// Fetch experiences for About section
+function fetchExperience() {
+  fetch("./data/experience.json")
+  .then((response) => response.json())
+  .then((data) => {
+    renderExperienceListContents(educationList, data.education);
+    renderExperienceListContents(workList, data.work);
+  })
+  .catch(error => console.error(error));
+}
+
+function renderExperienceListContents(list, data) {
+  data.forEach(item => {
+    const li = document.createElement("li");
+    li.classList.add("experience-list__item");
+    li.innerHTML = `
+          <div class="experience-list__title">${item.title}</div>
+          <div class="experience-list__place">${item.place}</div>
+          <div class="experience-list__location">${item.location}</div>
+          <div class="experience-list__date">${item.date}</div>
+    `;
+    list.appendChild(li);
+  });
 }
 
 fetchGitHubRepos();
+fetchExperience();
